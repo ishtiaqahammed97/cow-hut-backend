72130be6d7f6ca0f50244f34b8defe4752d5f9e2
ishtiaq <isah2080@ymail.com>
2024-01-14 20:16:51 +0600

    [DONE] cast error & validation error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,8 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { IGenericErrorMessage } from '../../interfaces/error';
 import ApiError from '../../errors/ApiError';
 import config from '../../config';
+import handleValidationError from '../../errors/handleValidationError';
+import handleCastError from '../../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (
   error,
@@ -14,8 +16,28 @@ const globalErrorHandler: ErrorRequestHandler = (
   let message = 'Something wrong';
   let errorMessages: IGenericErrorMessage[] = [];
 
-  if (error instanceof ApiError) {
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error?.name === 'CastError') {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ApiError) {
     statusCode = error?.statusCode;
+    message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
     message = error?.message;
     errorMessages = error?.message
       ? [
